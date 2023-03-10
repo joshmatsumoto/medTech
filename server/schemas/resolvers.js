1427cdb9b057cbb96e29afdb7cee8df0678dbede
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-03-10 11:46:28 -0800

    doctors resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -31,28 +31,41 @@ const resolvers = {
   },
   // Define the functions that will fulfill the mutations
   Mutation: {
-    addHospital: async (parent, { name, location, patientCount }) => {
+    createHospital: async (parent, { name, location, patientCount }) => {
       // Create and return the new Hospital object
       return await Hospital.create({ name, location, patientCount });
     },
-    addPatientByAdmin: async (parent, { name, age, gender }) => {
+    createPatient: async (parent, { name, age, gender }) => {
       // Create and return the new Patient object
       return await Patient.create({ name, age, gender });
     },
-    addDoctorByAdmin: async (parent, { name, department }) => {
+    createDoctor: async (parent, { _id, name, email, password, department, officeHours, officeLocation }) => {
       // Create and return the new Doctor object
-      return await Doctor.create({ name, department });
+      return await Doctor.create({  _id, name, email, password, department, officeHours, officeLocation  });
     },
-    addAdminByAdmin: async (parent, { name, specialty }) => {
+    createAdmin: async (parent, { name, specialty }) => {
       // Create and return the new Admin object
       return await Administrator.create({ name, specialty });
     },
-    createDoctor: async (parent, {name, location}) => {
-      const doctor = await Doctor.create(args);
-      return doctor;
+    updateDoctor: async (parent,{ doctorId }, context) => {
+      if (context.user) {
+        return await Doctor.findByIdAndUpdate(
+          { _id: doctorId },
+          { $addToSet: { patients: context.user._id } },
+          { new: true }
+        );
+    }
     },
-
-  },
+    deleteDoctor: async (parent, { doctorId }, context) => { 
+      if (context.user) {
+        return await Doctor.findByIdAndUpdate(
+          { _id: doctorId },
+          { $pull: { patients: context.user._id } },
+          { new: true }
+        );  
+    }
+    },
+    }, 
 };
 
 module.exports = resolvers;
