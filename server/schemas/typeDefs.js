ed2214fbe9d3b3e16d14d0798dc941e8ee35c27d
Ashley Sese <sese.ashrean@gmail.com>
2023-03-10 11:08:00 -0800

    added admin typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -42,26 +42,23 @@ const typeDefs = gql`
     admin(username: String!, password: String!): Admin
   }
 
-  
+
   type Mutation {
-<<<<<<< HEAD
-    # Set the required fields for new schools
-    addHospital(name: String!, location: String!, patientCount: Int!): Hospital
-    addAdmin(username:String!, password:String!): Admin
-=======
     login(email: String!, password: String!): Auth
-    addDoctor(name: String!, officeHours: String!, officeLocation: String!, doctorScore: Float!): Doctor
     createDoctor(name: String!, officeHours: String!, officeLocation: String!, doctorScore: Float): Doctor
     updateDoctor(id: ID!, name: String, officeHours: String, officeLocation: String doctorScore: Float): Doctor
-    deleteDoctor(id: ID!): Doctor 
+    deleteDoctor(id: ID!): Doctor
     getallDoctors: [Doctor]
     getallPatients: [Patient]
     createPatient(name: String!, department: String!, creditHours: Int!): Patient
     updatePatient(id: ID!, name: String, department: String, creditHours: Int): Patient
     deletePatient(id: ID!): Patient
+    createAdmin(name: String!, email: String!, username: String!, password: String!): Admin
+    updateAdmin(id: ID!, name: String, email: String, username: String, password: String): Admin
+    deleteAdmin(id: ID!): Admin
+    getallAdmin: [Admin]
 
 
->>>>>>> 30e878f141ebfa5a2944dfd1bc3892aa2653a3eb
   }
 `;
 
