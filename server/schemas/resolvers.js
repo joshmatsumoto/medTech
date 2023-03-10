44057be7c8db3f6573d19100b9dd2f77cfee5d73
Ashley Sese <sese.ashrean@gmail.com>
2023-03-10 10:47:38 -0800

    fixing error

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { Hospital, Patient, Doctor } = require('../models');
+const { Hospital, Patient, Doctor, Administrator } = require('../models');
 
 const resolvers = {
   Query: {
@@ -17,13 +17,35 @@ const resolvers = {
     doctors: async () => {
       return await Doctor.find({}).populate('patients');
     },
-    
+    admin: async () => {
+      return {
+        hospitals: await Hospital.find({}).populate('patients').populate({
+          path: 'patients',
+          populate: 'doctor',
+        }),
+        doctors: await Doctor.find({}).populate('patients'),
+        patients: await Patient.find({}).populate('doctor')
+      }
+    }
+
   },
   // Define the functions that will fulfill the mutations
   Mutation: {
     addHospital: async (parent, { name, location, patientCount }) => {
       // Create and return the new Hospital object
       return await Hospital.create({ name, location, patientCount });
+    },
+    addPatientByAdmin: async (parent, { name, age, gender }) => {
+      // Create and return the new Patient object
+      return await Patient.create({ name, age, gender });
+    },
+    addDoctorByAdmin: async (parent, { name, department }) => {
+      // Create and return the new Doctor object
+      return await Doctor.create({ name, department });
+    },
+    addAdminByAdmin: async (parent, { name, specialty }) => {
+      // Create and return the new Admin object
+      return await Administrator.create({ name, specialty });
     },
   },
 };
