7eadb64be5b43b7cca75903fe5efdf9283c6d8a8
Ashley Sese <sese.ashrean@gmail.com>
2023-03-11 00:39:24 -0800

    done for then night, working on Admin component still

--- client/src/components/AdminLogin/AdminLogin.js
+++ client/src/components/AdminLogin/AdminLogin.js
@@ -1,1 +1,5 @@
+import React, { useReducer, useState } from 'react';
 
+// import {ADD - Admin, Doctor, Remove - Admin, Doctor, } from '../utils/actions';
+// import reducer from '../utils/reducers';
+// import {useAdminContext} from '../utils/AdminContext';
