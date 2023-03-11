4edbfa5761504c989f5b183a45f7023851c64f54
Ashley Sese <sese.ashrean@gmail.com>
2023-03-11 15:27:28 -0800

    finished Admin Login

--- client/src/components/AdminLogin/AdminLogin.js
+++ client/src/components/AdminLogin/AdminLogin.js
@@ -1,26 +1,75 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-
-function AdminList() {
+// see SignupForm.js for comments
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+//import Auth from '../utils/auth';
+const AdminList = () => {
+  const [adminFormData, setAdminFormData] = useState({ email: '', password: '' });
+  const [validated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setAdminFormData({ ...adminFormData, [name]: value });
+  };
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    try {
+      const response = AdminList();
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
+     // const { token, doctor } = await response.json();
+     // console.log(doctor);
+      //Auth.login(token);
+//reset()
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+};
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          Please enter your email
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+          Something went wrong with your login credentials!
+        </Alert>
+        <Form.Group>
+          <Form.Label htmlFor='email'>Email</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Your email'
+            name='email'
+            onChange={handleInputChange}
+            value={adminFormData.email}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor='password'>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Your password'
+            name='password'
+            onChange={handleInputChange}
+            value={adminFormData.password}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+        </Form.Group>
+        <Button
+          disabled={!(adminFormData.email && adminFormData.password)}
+          type='submit'
+          variant='success'>
+          Submit
+        </Button>
+      </Form>
+    </>
   );
-}
-
+};
 export default AdminList;
