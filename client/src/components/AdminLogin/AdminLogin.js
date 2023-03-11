e09552b80a742700ae23f88660b8cfb232a676ed
Ashley Sese <sese.ashrean@gmail.com>
2023-03-11 12:41:24 -0800

    started working on adminLogin.js

--- client/src/components/AdminLogin/AdminLogin.js
+++ client/src/components/AdminLogin/AdminLogin.js
@@ -1,5 +1,26 @@
-import React, { useReducer, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-// import {ADD - Admin, Doctor, Remove - Admin, Doctor, } from '../utils/actions';
-// import reducer from '../utils/reducers';
-// import {useAdminContext} from '../utils/AdminContext';
+function AdminList() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          Please enter your email
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+export default AdminList;
