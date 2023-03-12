bff4aa58cff01f24e9c05eaccc7b5338344c4cdf
JCSH <joshmatsbux@gmail.com>
2023-03-11 19:29:06 -1000

    fixed error in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -19,14 +19,6 @@ const resolvers = {
       throw new AuthenticationError("You need to be logged in to view patient profile");
     },
 
-    // patientByName: async (parent, {name}) => {
-    //   const  params = {}
-    //   if (name) {
-    //     params.name = { $regex: name };
-    //   }
-    //   return await Patient.find(params).populate("doctor");
-    // },
-
     doctors: async () => {
       return await Doctor.find({}).populate("patients");
     },
@@ -54,41 +46,41 @@ const resolvers = {
   // Define the functions that will fulfill the mutations
   Mutation: {
     login: async (parent, { userType, email, password }) => {
-     
-      if (userType="patient"){
+
+      if (userType = "patient") {
         const patient = await Patient.findOne({ email });
         const correctPw = await patient.isCorrectPassword(password);
 
         if (!correctPw) {
           throw new AuthenticationError('Incorrect password');
         }
-  
+
         const token = signToken(patient);
-  
+
         return { token, patient };
       }
-      else if (userType="doctor"){
+      else if (userType = "doctor") {
         const doctor = await Doctor.findOne({ email });
         const correctPw = await doctor.isCorrectPassword(password);
 
         if (!correctPw) {
           throw new AuthenticationError('Incorrect password');
         }
-  
+
         const token = signToken(doctor);
-  
+
         return { token, doctor };
       }
-      else if (userType="administrator"){
+      else if (userType = "administrator") {
         const administrator = await Administrator.findOne({ email });
         const correctPw = await administrator.isCorrectPassword(password);
 
         if (!correctPw) {
           throw new AuthenticationError('Incorrect passwword');
         }
-  
+
         const token = signToken(administrator);
-  
+
         return { token, administrator };
       }
       else {
@@ -97,71 +89,67 @@ const resolvers = {
 
 
     }
-  }
-};
-
-    createPatient: async (parent, args, context) => {
-      const patient = await Patient.create(args);
-      const token = signToken(patient);
-      return { token, patient };
-    },
-
-    createDoctor: async (parent, args, context) => {
-      const doctor = await Doctor.create(args);
-      const token = signToken(doctor);
-      return { token, doctor };
-    },
-
-    createAdmin: async (parent, args, context) => {
-      const administrator = await Administrator.create(args);
-      const token = signToken(administrator);
-      return { token, administrator };
-    },
-
-    // Update Patient section
-    updatePatient: async (parent, { _id, name, age, gender, adress, phone, email, password }) => {
-      return Patient.findByIdAndUpdate(
-        _id,
-        { $set: { name, age, gender, adress, phone, email, password } },
-        { new: true }
-      );
-    },
-
-    // Update Doctor section
-    updateDoctor: async (parent, { _id, name, email, password, department, officeHours, officeLocation }) => {
-      return Doctor.findByIdAndUpdate(
-        _id,
-        { $set: { name, email, password, department, officeHours, officeLocation } },
-        { new: true }
-      );
-    },
-
-    // Update Admin section
-    updateAdmin: async (parent, { _id, name, email, password, phoneNumber }) => {
-      return await Administrator.findByIdAndUpdate(
-        _id,
-        { $set: { name, email, password, phoneNumber } },
-        { new: true }
-      );
-    },
-
-    // Delete Patient section
-    deletePatient: async (parent, { _id }) => {
-      return Patient.findByIdAndDelete(_id);
-    },
-
-    // Delete Doctor section
-    deleteDoctor: async (parent, { _id }) => {
-      return await Doctor.findByIdAndDelete(_id);
-    },
-
-    // Delete Admin Section
-    deleteAdmin: async (parent, { _id }) => {
-      return Administrator.findByIdAndDelete(_id);
-    },
-
   },
 
+  createPatient: async (parent, args, context) => {
+    const patient = await Patient.create(args);
+    const token = signToken(patient);
+    return { token, patient };
+  },
+
+  createDoctor: async (parent, args, context) => {
+    const doctor = await Doctor.create(args);
+    const token = signToken(doctor);
+    return { token, doctor };
+  },
+
+  createAdmin: async (parent, args, context) => {
+    const administrator = await Administrator.create(args);
+    const token = signToken(administrator);
+    return { token, administrator };
+  },
+
+  // Update Patient section
+  updatePatient: async (parent, { _id, name, age, gender, adress, phone, email, password }) => {
+    return Patient.findByIdAndUpdate(
+      _id,
+      { $set: { name, age, gender, adress, phone, email, password } },
+      { new: true }
+    );
+  },
+
+  // Update Doctor section
+  updateDoctor: async (parent, { _id, name, email, password, department, officeHours, officeLocation }) => {
+    return Doctor.findByIdAndUpdate(
+      _id,
+      { $set: { name, email, password, department, officeHours, officeLocation } },
+      { new: true }
+    );
+  },
+
+  // Update Admin section
+  updateAdmin: async (parent, { _id, name, email, password, phoneNumber }) => {
+    return await Administrator.findByIdAndUpdate(
+      _id,
+      { $set: { name, email, password, phoneNumber } },
+      { new: true }
+    );
+  },
+
+  // Delete Patient section
+  deletePatient: async (parent, { _id }) => {
+    return Patient.findByIdAndDelete(_id);
+  },
+
+  // Delete Doctor section
+  deleteDoctor: async (parent, { _id }) => {
+    return await Doctor.findByIdAndDelete(_id);
+  },
+
+  // Delete Admin Section
+  deleteAdmin: async (parent, { _id }) => {
+    return Administrator.findByIdAndDelete(_id);
+  },
 
 };
 
