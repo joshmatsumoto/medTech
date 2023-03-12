f7048ee54b673d51ef8081aa02cbc7f2bd9c01da
captaiiinsolo <solo.d.santos@gmail.com>
2023-03-12 15:31:10 -0700

    pull in most recent updates

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,14 @@ import "react-datepicker/dist/react-datepicker.css";
 import "./styles/custom.min.css";
 import Header from "./components/Header/Header";
 import AppointmentForm from "./components/AppointmentForm/AppointmentForm";
-import NewAdmin from "./components/NewAdmin/NewAdmin";
+import AdminLogin from "./components/AdminLogin/AdminLogin";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AdminLogin />
       <AppointmentForm />
-      <NewAdmin />
 
     </div>
     );
