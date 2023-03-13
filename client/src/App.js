770a28b7008aac3cb587a098a2706564bfe85b82
Ashley Sese <sese.ashrean@gmail.com>
2023-03-13 16:56:04 -0700

    updating app

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,12 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/custom.min.css';
 import Header from './components/Header/Header';
-import NewPatientForm from './components/NewPatientForm/NewPatientForm';
+import AdminDash from './pages/AdminDash';
 function App() {
   return (
     <div className="App">
       <Header />
-      <NewPatientForm />
+      <AdminDash />
       </div>
       );
 }
