16e60853ea76243f5dedff1b2e427027dccbfb96
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-03-13 23:05:09 -0700

    added log in body to the homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,10 +6,26 @@ const Home = () => {
     const posts = data?.posts || [];
     return (
        <>
-       <container fluid>
+       <Container fluid>
         <img src={medTechLogo2} alt="medTech Logo" className="App-logo" /> 
-       </>
-    )
+        <h1 className="App-title">MedTech</h1>
+        </Container>
+        <Container className='text-dark text-start'>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+                    Something went wrong with your login or password!   
+                </Alert>
+        <div>
+        <Link to="/PatientLogin">Patient Login</Link>
+        <PatientLogin />
+       </div>        
+       </Container>
+       <Container Fluid>
+
+
+       </Container>
+     </>  
+    );
 };
 
 
