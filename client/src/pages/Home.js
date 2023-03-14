5a51b350710b26ecf51c6b510c5d72a08f1fc2c0
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-03-13 23:38:08 -0700

    updated  log in body to the homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,31 +1,34 @@
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-    const { loading, data } = useQuery();
-    fet
-    const posts = data?.posts || [];
+
     return (
-       <>
-       <Container fluid>
-        <img src={medTechLogo2} alt="medTech Logo" className="App-logo" /> 
-        <h1 className="App-title">MedTech</h1>
+        <>
+        <Container fluid>
+         <img src={medTechLogo2} alt="medTech Logo" className="App-logo" /> 
+         <h1 className="App-title">MedTech</h1>
+         </Container>
+     
+         <Form noValidate validated={validated} onSubmit={handleSubmit}>
+             {showAlert && ( 
+                 <Alert dismissible onClose={() => setShowAlert(false)} variant='danger'>
+                     Something went wrong with your login or password!   
+                 </Alert> 
+             )}
+     
+             <div className='text-dark text-start'> 
+                 <Link to="/PatientLogin">Patient Login</Link> 
+                 <PatientLogin/>  
+             </div>        
+     
+         </Form>
+     
+        <Container Fluid>
+        <img src={medTechLogo} alt="medTech Logo" />
         </Container>
-        <Container className='text-dark text-start'>
-            <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-                    Something went wrong with your login or password!   
-                </Alert>
-        <div>
-        <Link to="/PatientLogin">Patient Login</Link>
-        <PatientLogin />
-       </div>        
-       </Container>
-       <Container Fluid>
-
-
-       </Container>
-     </>  
-    );
+     
+      </>  
+     );
 };
 
 
