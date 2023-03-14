004fe2c016ea34ab7d39d64c78551a9f761476d3
captaiiinsolo <solo.d.santos@gmail.com>
2023-03-14 12:32:29 -0700

    fix nav

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,9 @@
 
 import React from "react";
-import Footer from "../components/Footer/Footer";
 
 function Home() {
     return (
         <div className="Home">
-            <Footer />
         </div>
     )
 }
