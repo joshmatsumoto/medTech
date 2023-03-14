20c9f04487ccdda32f74faf84bec9b36e8a8306f
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-03-14 13:18:21 -0700

    created safety tips file

---
+++ client/src/components/Tips.js/Tips.js
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import AdminLogin from '../AdminLogin/AdminLogin';
+import DoctorLogin from '../DoctorLogin/DoctorLogin';
+import {Col, Row, Button, Modal} from 'react-bootstrap';
