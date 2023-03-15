1d786598051b1a5e1b876f4b12da639b9b30f543
captaiiinsolo <solo.d.santos@gmail.com>
2023-03-15 10:00:57 -0700

    write initial code for doctor navigation bar

--- client/src/components/Navigation/AdminNavigation.js
+++ client/src/components/Navigation/AdminNavigation.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import NewAdminForm from '../NewAdminForm/NewAdminForm'
+import NewAdminForm from '../NewAdminForm/NewAdminForm';
 
 function AdminNavBar(props) {
   const [showAdminForm, setShowAdminForm] = useState(false);
---
+++ client/src/components/Navigation/DoctorNavigation.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import NewAdminForm from '../NewAdminForm/NewAdminForm';
+
+function DoctorNavBar() {
+    const [showForm, setShowForm] = useState(false);
+
+    const handleShowDocNav = () => {
+        setShowDocNav(true);
+};
+
+return (
+    <div>
+        
+    </div>
+
+);
+
+export default DoctorNavBar;
