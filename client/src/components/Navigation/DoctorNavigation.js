d1b4a1829ed7b7cf9d7354a9f30b36725d818950
captaiiinsolo <solo.d.santos@gmail.com>
2023-03-15 10:31:07 -0700

    add dropdown items to doctor nav

--- client/src/components/Navigation/DoctorNavigation.js
+++ client/src/components/Navigation/DoctorNavigation.js
@@ -3,7 +3,9 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import NewAdminForm from '../NewAdminForm/NewAdminForm';
+import { Link } from'react-router-dom';
+import NewDoctorForm from '../NewDoctorForm';
+import NewPatientForm from '../NewPatientForm';
 
 function DoctorNavBar() {
     const [showForm, setShowForm] = useState(false);
@@ -14,7 +16,35 @@ function DoctorNavBar() {
 
 return (
     <div>
-        
+        <Navbar bg="tertiary" variant="dark">
+            <Container fluid>
+                <Navbar.Brand className='justify-content-between'>
+                    MedTech
+                </Navbar.Brand>
+                <Nav>
+                    <Nav.Link as={Link} to="/">
+                        Home
+                    </Nav.Link>
+                    <Nav.NavDropdown title="Appointments" id="appointment-dropdown">
+                        <NavDropdown.Item>
+                            Create New Appointment
+                        </NavDropdown.Item>
+                        <NavDropdown.Item>
+                            View All Appointments
+                        </NavDropdown.Item>
+                    <Nav.NavDropdown title="Patients" id="patients-dropdown">
+                        <NavDropdown.Item>
+                            Create New Patient
+                        </NavDropdown.Item>
+                        <NavDropdown.Item>
+                            View All Patients
+                        </NavDropdown.Item>
+                    </Nav.NavDropdown>
+                    </Nav.NavDropdown>
+
+                </Nav>
+            </Container>
+        </Navbar>
     </div>
 
 );
