482c7d9ceb479bb2d1ae9ea9ad74ce81fbe38a8e
JCSH <joshmatsbux@gmail.com>
2023-03-15 16:24:24 -1000

    added images into safety tips

--- client/src/components/SafetyTips/SafetyTips.js
+++ client/src/components/SafetyTips/SafetyTips.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import Image from 'react-bootstrap/Image'
-import safetynet from '../assets/safetynet.png';
-import medTechLogoNoWhite from '../assets/medTechLogoNoWhite.png';
-import Edwin from '../assets/Edwin.jpg';
-import group from '../assets/group.jpg';
+import safetynet from '../../assets/safetynet.png';
+import Edwin from '../../assets/Edwin.jpg';
+import group from '../../assets/group.jpg';
 import { Link } from'react-router-dom';
 
 function SafetyTips() {
   return (
     <Carousel variant="dark">
       <Carousel.Item interval={1000}>
-        <div> < Image as={Link} to="https://www.techsafety.org/technology-safety-quick-tips" width={900} height={500} alt="1x2"
+        <div> < Image as={Link} to="https://www.techsafety.org/technology-safety-quick-tips" alt="1x2"
           className="d-block w-100"
           src={safetynet}/>
           </div>
@@ -20,7 +19,7 @@ function SafetyTips() {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={1000}>
-      <div> <Image as={Link} to="https://giving.aidslifecycle.org/participant/2549" width={1} height={2} alt="1x2"
+      <div> <Image as={Link} to="https://giving.aidslifecycle.org/participant/2549"  alt="1x2"
           className="d-block w-100"
           src={Edwin}
         /> </div>
@@ -29,7 +28,7 @@ function SafetyTips() {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <div> <Image as={Link} to="https://giving.aidslifecycle.org/participant/2549" width={1} height={2} alt="1x2"
+      <div> <Image as={Link} to="https://giving.aidslifecycle.org/participant/2549" alt="1x2"
           className="d-block w-100"
           src={group}
         /> </div>
