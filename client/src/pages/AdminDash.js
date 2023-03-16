a067c7e728a6bc41e4667a59340524155f217bc8
JCSH <joshmatsbux@gmail.com>
2023-03-16 07:06:24 -1000

    ahh

---
+++ client/src/pages/AdminDash.js
@@ -0,0 +1,17 @@
+import DoctorNavBar from "../components/Navigation/DoctorNavigation";
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+const AdminDashboardNavs = () =>{
+    return (
+      <>
+        <Header/>
+        <DoctorNavBar/>
+        <Footer/>
+      </>
+      );
+
+  };
+
+  
+  export default AdminDashboardNavs;
